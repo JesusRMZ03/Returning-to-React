88bcf8d604e494c43afaaf06f9a41653cf255d61
JesusRMZ03 <A01022447@itesm.mx>
2021-04-13 14:59:38 -0500

    changing react app

---
+++ 02-intro-javascript/src/bases/01-VariblesyContantes.js
@@ -0,0 +1,14 @@
+//Varaibles y constantes
+
+const nombre ='Jesus'; 
+const apellido = 'Ramirez';
+
+let valorDado = 5 ; //lo dejo como let si se que va cambiar el valor
+
+console.log(nombre, apellido , valorDado)
+
+if (true) {
+    let valorDado = 6;
+
+    console.log(valorDado)
+}
